1b1c47afebc31d13e55e736c882096f61b4db0d3
wjchulme <25637345+wjchulme@users.noreply.github.com>
2021-10-07 17:01:34 +0100

    add region-specific plots

--- analysis/descriptive/event_date.R
+++ analysis/descriptive/event_date.R
@@ -41,6 +41,7 @@ data_tte <- data_cohort %>%
     vax1_day,
     vax1_type,
     vax1_type_descr,
+    region,
 
     end_date,
     censor_date = pmin(
@@ -74,7 +75,7 @@ data_tte <- data_cohort %>%
 alltimes <- expand(data_tte, patient_id, times=as.integer(full_seq(c(1, tte_censor),1)))
 
 data_pt <- tmerge(
-  data1 = data_tte %>% select(-starts_with("ind_"), -ends_with("_date"), vax1_date),
+  data1 = data_tte,
   data2 = data_tte,
   id = patient_id,
   tstart = 0L,
@@ -143,8 +144,8 @@ event_rates <-
   geom_hline(aes(yintercept=0), colour='black')+
  # geom_step(aes(x=date_week, y=rate, colour=vax1_type_descr), size=1)+
   geom_segment(aes(x=date_week, y=rate, xend=date_week+7, yend=rate, colour=vax1_type_descr), size=1)+
-  geom_segment(aes(x=date_week+7, y=rate, xend=date_week+7, yend=rate_end, colour=vax1_type_descr), size=1)+
-  facet_grid(rows=vars(outcome_descr_wrap), switch="y", scales = "free_y")+
+  geom_segment(aes(x=date_week+7, y=rate, xend=date_week+7, yend=rate_end, colour=vax1_type_descr), size=0.5)+
+  facet_grid(cols=vars(outcome_descr_wrap), switch="y", scales = "free_y")+
   scale_x_date(
     breaks = seq(min(data_by_day$date_week), max(data_by_day$date_week)+15,by=28),
     limits = c(lubridate::floor_date(min(data_by_day$date_week), "1 month"), NA),
@@ -181,3 +182,98 @@ write_rds(event_rates, here("output", "descriptive", "eventdate", "eventdate_ste
 ggsave(event_rates, filename="plot_eventdate_step.png", path=here("output", "descriptive", "eventdate"))
 ggsave(event_rates, filename="plot_eventdate_step.svg", path=here("output", "descriptive", "eventdate"))
 ggsave(event_rates, filename="plot_eventdate_step.pdf", path=here("output", "descriptive", "eventdate"))
+
+
+
+
+data_by_day_region <-
+  data_pt %>%
+  group_by(date_week,region,vax1_type_descr) %>%
+  summarise(
+    #vax1 = mean(vax1),
+    #underfup = n(),
+    postest=weighted.mean(postest, 1-status_postest),
+    covidemergency=weighted.mean(covidemergency, 1-status_covidemergency),
+    covidadmitted=weighted.mean(covidadmitted, 1-status_covidadmitted),
+
+  ) %>%
+  pivot_longer(
+    cols=c(postest, covidemergency,covidadmitted),
+    names_to="outcome",
+    values_to="rate"
+  ) %>%
+  left_join(
+    metadata_outcomes, by="outcome"
+  ) %>%
+  mutate(
+    outcome_descr=fct_inorder(outcome_descr),
+    outcome_descr_wrap=fct_inorder(str_wrap(outcome_descr,15))
+  ) %>%
+  group_by(outcome_descr,region,vax1_type_descr) %>%
+  mutate(
+    rate_end=lead(rate)
+  )
+
+plot_outcome <- function(outcome){
+  outcomee <- outcome
+
+    data_by_day_region %>%
+    bind_rows(data_by_day %>% mutate(region="England")) %>%
+    ungroup() %>%
+    filter(outcome==outcomee) %>%
+    mutate(region=fct_inorder(region)) %>%
+    ggplot()+
+    geom_hline(aes(yintercept=0), colour='black')+
+    #geom_step(aes(x=date_week, y=rate, colour=region), size=1)+
+    #geom_line(aes(x=date_week+3.5, y=rate, colour=region), size=1)+
+    geom_segment(aes(x=date_week, y=rate, xend=date_week+7, yend=rate, colour=vax1_type_descr), size=1)+
+    geom_segment(aes(x=date_week+7, y=rate, xend=date_week+7, yend=rate_end, colour=vax1_type_descr), size=0.2)+
+    facet_wrap(vars(region), scales = "fixed", ncol=2, dir="v")+
+    scale_x_date(
+      breaks = seq(min(data_by_day$date_week), max(data_by_day$date_week)+15,by=7*4),
+      limits = c(lubridate::floor_date(min(data_by_day$date_week), "1 month"), NA),
+      labels = scales::label_date("%d/%m"),
+      expand = expansion(add=3),
+      sec.axis = sec_axis(
+        trans = ~as.Date(.),
+        breaks=as.Date(seq(floor_date(min(data_by_day$date_week), "month"), ceiling_date(max(data_by_day$date_week), "month"),by="month")),
+        labels = scales::label_date("%b %y")
+      )
+    )+
+    scale_y_continuous(expand=expansion(mult=c(0,0.05)), labels=scales::label_number(0.01, scale=1000))+
+    scale_colour_brewer(type="qual", palette="Set1")+
+    labs(
+      x="Date",
+      y="Incidence per 1000 people",
+      colour=NULL
+    )+
+    theme_minimal()+
+    theme(
+      legend.position = c(0.97,0.97),
+      legend.justification = c(1,1),
+      strip.placement = "outside",
+      strip.text.y.left = element_text(angle = 0, hjust=1),
+      axis.text.x.top = element_text(hjust=0),
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank(),
+      axis.ticks.x = element_line(colour = 'black')
+    )
+
+}
+
+plot_postest <- plot_outcome("postest")
+plot_covidemergency <- plot_outcome("covidemergency")
+plot_covidadmitted <- plot_outcome("covidadmitted")
+
+ggsave(plot_postest, filename="plot_region_postest_step.png", path=here("output", "descriptive", "eventdate"))
+ggsave(plot_postest, filename="plot_region_postest_step.svg", path=here("output", "descriptive", "eventdate"))
+ggsave(plot_postest, filename="plot_region_postest_step.pdf", path=here("output", "descriptive", "eventdate"))
+
+ggsave(plot_covidemergency, filename="plot_region_covidemergency_step.png", path=here("output", "descriptive", "eventdate"))
+ggsave(plot_covidemergency, filename="plot_region_covidemergency_step.svg", path=here("output", "descriptive", "eventdate"))
+ggsave(plot_covidemergency, filename="plot_region_covidemergency_step.pdf", path=here("output", "descriptive", "eventdate"))
+
+ggsave(plot_covidadmitted, filename="plot_region_covidadmitted_step.png", path=here("output", "descriptive", "eventdate"))
+ggsave(plot_covidadmitted, filename="plot_region_covidadmitted_step.svg", path=here("output", "descriptive", "eventdate"))
+ggsave(plot_covidadmitted, filename="plot_region_covidadmitted_step.pdf", path=here("output", "descriptive", "eventdate"))
+
